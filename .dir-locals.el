060c2de1042a5b14448e0ba7d14d7466d5eea26a
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-09-28 20:22:50 +0300

    .dir-locals.el:  provision for dante

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+((nil
+  .(
+    (dante-repl-command-line "cabal" "repl" "lib:reflex-glfw")
+    (haskell-interactive-tgt              . "lib:reflex-glfw")
+    (dante-target                         . "lib:reflex-glfw")
+    (dante-project-root .                     "~/reflex-glfw/src")
+    )))
