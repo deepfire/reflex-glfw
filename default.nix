8097747ed1c665adab70003172838fcbfe2e3a7b
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-09-30 18:12:07 +0300

    default.nix:  regenerate

--- default.nix
+++ default.nix
@@ -1,17 +1,16 @@
-{ mkDerivation, base, base-unicode-symbols, containers, dependent-sum
-, GLFW-b, lens, mtl, OpenGL, OpenGLRaw, pretty, reflex, stdenv, stm
-, transformers
+{ mkDerivation, base, base-unicode-symbols, containers
+, dependent-sum, GLFW-b, lens, mtl, OpenGL, OpenGLRaw, pretty
+, primitive, ref-tf, reflex, stdenv, stm, transformers
 }:
-
 mkDerivation {
   pname = "reflex-glfw";
-  version = "0.1.0.0";
+  version = "0.1.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
   libraryHaskellDepends = [
     base base-unicode-symbols containers dependent-sum GLFW-b lens mtl
-    OpenGLRaw pretty reflex stm transformers
+    OpenGLRaw pretty primitive ref-tf reflex stm transformers
   ];
   executableHaskellDepends = [
     base base-unicode-symbols containers dependent-sum GLFW-b lens mtl
