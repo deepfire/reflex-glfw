635612e7156f4c8ac4718cdf6c3806257f40f827
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2018-10-14 19:46:40 +0300

    Demo:  inline Gear.hs

--- Demo.hs
+++ Demo.hs
@@ -22,7 +22,7 @@ module Main (main) where
 import Control.Arrow             ((***))
 import Control.Concurrent.STM    (TQueue, atomically, newTQueueIO, tryReadTQueue, writeTQueue)
 import Control.Lens
-import Control.Monad             (join, unless, void, when)
+import Control.Monad             (join, unless, void, when, forM_)
 import Control.Monad.IO.Class    (MonadIO, liftIO)
 import Control.Monad.RWS.Strict  (RWST, ask, asks, evalRWST, get, liftIO, modify, put)
 import Control.Monad.Trans.Maybe (MaybeT(..), runMaybeT)
@@ -40,8 +40,6 @@ import qualified System.IO                         as Sys
 
 import           Reflex
 import           Reflex.GLFW                       as RGL
-
-import Gear (makeGear)
 
 
 data Env = Env
@@ -487,3 +485,119 @@ joysticks =
   , GLFW.Joystick'15
   , GLFW.Joystick'16
   ]
+
+
+makeGear ∷ Float → Float → Float → Int → Float → GL.Color4 GL.GLfloat → IO GL.DisplayList
+makeGear inradius outradius width teeth toothdepth color =
+    GL.defineNewList GL.Compile $ do
+        GL.colorMaterial GL.$= Just (GL.Front, GL.AmbientAndDiffuse)
+        GL.color color
+        drawGear
+  where
+    drawGear = do
+        let r0   = inradius
+            r1   = outradius - toothdepth / 2
+            r2   = outradius + toothdepth / 2
+            pi2  = 2 * pi
+            da   = pi2 / realToFrac teeth / 4 ∷ Float
+            wd2  = width * 0.5
+            wd2n = negate wd2
+
+            calca ∷ Int → Float
+            calca i = realToFrac i * pi2 / realToFrac teeth ∷ Float
+
+        GL.shadeModel GL.$= GL.Flat
+        normal 0 0 1
+
+        -- front face
+        GL.renderPrimitive GL.QuadStrip $
+            forM_ [0 .. teeth] $ \i → do
+                let a = calca i
+                vertex (r0 * cos a) (r0 * sin a) wd2
+                vertex (r1 * cos a) (r1 * sin a) wd2
+                when (i < teeth) $ do
+                    vertex (r0 * cos a) (r0 * sin a) wd2
+                    vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2
+
+        -- front sides of teeth
+        GL.renderPrimitive GL.Quads $
+            forM_ [0 .. pred teeth] $ \i → do
+                let a = calca i
+                vertex (r1 * cos a) (r1 * sin a) wd2
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2
+
+        normal 0 0 (-1)
+
+        -- back face
+        GL.renderPrimitive GL.QuadStrip $
+            forM_ [0 .. teeth] $ \i → do
+                let a = calca i
+                vertex (r1 * cos a) (r1 * sin a) wd2n
+                vertex (r0 * cos a) (r0 * sin a) wd2n
+                when (i < teeth) $ do
+                    vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2n
+                    vertex (r0 * cos a) (r0 * sin a) wd2n
+
+        -- back sides of teeth
+        GL.renderPrimitive GL.Quads $
+            forM_ [0 .. pred teeth] $ \i → do
+                let a = calca i
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2n
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2n
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2n
+                vertex (r1 * cos a) (r1 * sin a) wd2n
+
+        -- outward faces of teeth
+        GL.renderPrimitive GL.QuadStrip $ do
+            forM_ [0 .. pred teeth] $ \i → do
+                let a = calca i
+                vertex (r1 * cos a) (r1 * sin a) wd2
+                vertex (r1 * cos a) (r1 * sin a) wd2n
+                let u = r2 * cos (a + da) - r1 * cos a
+                    v = r2 * sin (a + da) - r1 * sin a
+                    len = sqrt (u * u + v * v)
+                    un = u / len
+                    vn = v / len
+                normal vn (-un) 0
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2
+                vertex (r2 * cos (a + da)) (r2 * sin (a + da)) wd2n
+                normal (cos a) (sin a) 0
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2
+                vertex (r2 * cos (a + 2 * da)) (r2 * sin (a + 2 * da)) wd2n
+                let u' = r1 * cos (a + 3 * da) - r2 * cos (a + 2 * da)
+                    v' = r1 * sin (a + 3 * da) - r2 * sin (a + 2 * da)
+                normal v' (-u') 0
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2
+                vertex (r1 * cos (a + 3 * da)) (r1 * sin (a + 3 * da)) wd2n
+                normal (cos a) (sin a) 0
+
+            vertex (r1 * cos 0) (r1 * sin 0) wd2
+            vertex (r1 * cos 0) (r1 * sin 0) wd2n
+
+        GL.shadeModel GL.$= GL.Smooth
+
+        -- inside radius cylinder
+        GL.renderPrimitive GL.QuadStrip $
+            forM_ [0 .. teeth] $ \i → do
+                let a = calca i
+                normal (negate $ cos a) (negate $ sin a) 0
+                vertex (r0 * cos a) (r0 * sin a) wd2n
+                vertex (r0 * cos a) (r0 * sin a) wd2
+
+vertex ∷ Float → Float → Float → IO ()
+vertex x y z =
+    GL.vertex $ mkVertex x y z
+
+mkVertex ∷ Float → Float → Float → GL.Vertex3 GL.GLfloat
+mkVertex x y z =
+    GL.Vertex3 (realToFrac x) (realToFrac y) (realToFrac z)
+
+normal ∷ Float → Float → Float → IO ()
+normal x y z =
+    GL.normal $ mkNormal x y z
+
+mkNormal ∷ Float → Float → Float → GL.Normal3 GL.GLfloat
+mkNormal x y z =
+    GL.Normal3 (realToFrac x) (realToFrac y) (realToFrac z)
